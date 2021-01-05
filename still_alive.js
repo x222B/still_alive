9396d09b587f7b1714a7681514232d6f576ca2f6
Dino Tahto <dinotahto.dev@gmail.com>
2021-01-05 18:31:22 +0100

    Initial commit

---
+++ still_alive.js
@@ -0,0 +1,66 @@
+const net = require('net');
+
+const options = {
+    hostname : '127.0.0.1',
+    port: 8080,
+    path: '/',
+    method: 'GET',
+    maxSockets: 200,
+    respawn: true,
+    rate:500
+}
+
+let aliveSockets=0;
+
+function createSocket(){
+
+    let socket = new net.Socket();
+
+    socket.connect(options.port,options.port);
+
+    let rand=Math.floor(Math.random()*100);
+    let sentPackages=0;
+
+    function stillAlive(){
+        sentPackages++;
+        socket.write('Socket[' + rand + ']:Package['+ (sentPackages * rand) + ']', ()=>{
+            setTimeout(()=>{
+                stillAlive!==false ? stillAlive() : null,
+                options.rate
+            })
+        });
+    }
+
+    socket.on('connect',()=>{
+        socket.write(options.method + ' ' + options.path +' HTTP/1.1\n', 'ascii', ()=>{
+            console.log('New socket created. Active sockets: ['+ aliveSockets + ']');
+            aliveSockets++;
+            socket.write('Initial connection to ' + options.hostname + ':' + options.port);
+
+            stillAlive();
+        })
+    })
+
+    socket.on('error',(err)=>{
+        console.error('Socket Error: ' + err.message)
+        socket.destroy();
+    })
+
+    socket.on('data',(data)=>{
+        console.log(data.toString());
+    })
+
+    socket.on('close',()=>{
+        aliveSockets--;
+        stillAlive = false
+
+        if (options.respawn=true){
+            console.log('Respawning dead socket.')
+            createSocket();
+        }
+    })
+}
+
+for(let i = 0; i<options.maxSockets; i++){
+    createSocket();
+}
