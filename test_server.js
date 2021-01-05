dddda16d4c33c41cd46d1dcf9cab2cd81e7c4359
Dino Tahto <dinotahto.dev@gmail.com>
2021-01-05 18:57:13 +0100

    Initial commit

---
+++ test_server.js
@@ -0,0 +1,24 @@
+var net = require('net');
+
+let activeSockets=[];
+let receivedPackets=0;
+
+console.log('')
+
+var server = net.Server((socket)=>{
+    activeSockets.push(socket);
+
+    socket.on('data',(data)=>{
+        receivedPackets++;
+        console.log('Active Sockets: ',activeSockets.length);
+        console.log('Received Packets ['+ receivedPackets +']');
+    })
+});
+
+server.on('error', (err) => {
+  throw err;
+});
+
+server.listen(8080, '127.0.0.1',()=>{
+    console.log('Server listening on 127.0.0.1:8080');
+});
